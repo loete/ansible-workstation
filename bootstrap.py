7f604cb2bd3f99542352c1692070417b4301fd5a
Stefan L <stefan.ltnr@gmail.com>
2018-12-07 22:38:04 +0100

    updated zsh stuff

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import subprocess, os
-# Check if Ansible installation - install if not 
-packages = 'software-properties-common ansible git python-apt' 
+# Check if Ansible installation - install if not
+packages = 'software-properties-common ansible git python-apt'
 
 print("Checking Ansible installation")
 apt_list = subprocess.Popen(['apt', 'list', '--installed'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -10,10 +10,10 @@ try:
     print("- Ansible already installed!")
 except:
     print("- Ansible not found - installing ...")
-    print("  - apt-get update")
-    os.system('sudo apt-get update > /dev/null')
-    print("  - apt-get install")
-    os.system('sudo apt-get install {} -y > /dev/null'.format(packages))
+    print("  - apt update")
+    os.system('sudo apt update > /dev/null')
+    print("  - apt install")
+    os.system('sudo ap install {} -y > /dev/null'.format(packages))
     print("- Ansible installed!")
 
 # Running Ansible playbook
